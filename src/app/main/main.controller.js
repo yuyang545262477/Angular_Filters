a0a0eb301ccbab5ef5d8a48908c1670ba32168fa
David_Yu <yuyang545262477@gmail.com>
2016-06-26 22:12:18 +0800

    write controller add some datas

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,39 +1,27 @@
-(function() {
-  'use strict';
-
-  angular
+(function () {
+    'use strict';
+    
+    angular
     .module('webstorm')
     .controller('MainController', MainController);
-
-  /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
-    var vm = this;
-
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1466948474704;
-    vm.showToastr = showToastr;
-
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
+    
+    function MainController() {
+        var vm = this;
+        vm.data = [
+            {name: "Apples", category: "Fruit", price: 1.20, expiry: 10},
+            {name: "Bananas", category: "Fruit", price: 2.42, expiry: 7},
+            {name: "Pears", category: "Fruit", price: 2.02, expiry: 6},
+            
+            {name: "Tuna", category: "Fish", price: 20.45, expiry: 3},
+            {name: "Salmon", category: "Fish", price: 17.93, expiry: 2},
+            {name: "Trout", category: "Fish", price: 12.93, expiry: 4},
+            
+            {name: "Beer", category: "Drinks", price: 2.99, expiry: 365},
+            {name: "Wine", category: "Drinks", price: 8.99, expiry: 365},
+            {name: "Whiskey", category: "Drinks", price: 45.99, expiry: 365}
+        ];
+        
+        
     }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
-  }
+    
 })();
